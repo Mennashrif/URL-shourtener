da1e43c0b37bc8351c0c19000c7fe21f788810f0
Mennashrif <49440806+Mennashrif@users.noreply.github.com>
2021-09-08 19:39:45 +0200

    Finish-Angular-Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
-import { FormComponent } from './form/form.component';
-import { HeaderSectionComponent } from './header-section/header-section.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SectionComponent,
-    FormComponent,
-    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/section/section.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl:'./section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent {
+  URl=""
+        onKeyUp(){
+          console.log(this.URl)
+        }
+
+}
